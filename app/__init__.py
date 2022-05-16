14385a9c6b55712926f58163933428925997e4da
timmy <timothy.ndung'u@student.moringaschool.com>
2022-05-17 01:27:01 +0300

    login manager

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,11 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
-# from config import config_options
-# from flask_sqlalchemy import SQLAlchemy
-# from flask_uploads import UploadSet,configure_uploads,IMAGES
+from config import config_options
+from flask_sqlalchemy import SQLAlchemy
+from flask_uploads import UploadSet,configure_uploads,IMAGES
 from flask_mail import Mail
-# from flask_login import LoginManager
-# from flask_simplemde import SimpleMDE
+from flask_login import LoginManager
+from flask_simplemde import SimpleMDE
 
 
 
@@ -17,7 +17,7 @@ simple = SimpleMDE()
 
 
 
-login_manager = LoginManager() 
+login_manager = LoginManager()
 login_manager.session_protection = 'strong' 
 login_manager.login_view = 'auth.login'
 
